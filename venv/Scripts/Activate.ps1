ff79de84d18f929d2432bda21f5a2f2905a417c3
alexinnes <alexnixoninnes@gmail.com>
2019-01-15 21:53:10 +0000

    On tutorial 4 now

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\ainnes\Google Drive\Scripts\Python\Projects\flask_testing\venv"
+$env:VIRTUAL_ENV="C:\Users\Alex\Google Drive\Scripts\Python\Projects\flask_testing\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
